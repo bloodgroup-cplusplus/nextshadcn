b493d208aabc43da4cca28265a6c7a54c19d268c
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-05 02:39:56 +0530

    added components

---
+++ src/app/login/page.js
@@ -0,0 +1,9 @@
+'use client'
+import {Label} from "../../../components/ui/label"
+
+export default function page()
+{
+    return (
+        <Label htmlFor="email">Your email address</Label>
+    )
+}
